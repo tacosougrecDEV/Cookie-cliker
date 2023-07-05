60420e54f196f72d30f541426b0b1ab50e5ab94c
tacosougrecDEV <103994693+tacosougrecDEV@users.noreply.github.com>
2023-07-05 02:26:37 +0200

    Update Rate.jsx
    .

--- src/Rate.jsx
+++ src/Rate.jsx
@@ -3,7 +3,7 @@ import cookie from "./assets/img/cookie.png";
 
 const Rate = ({bonus}) => {
   return (
-    <div className="w-fit py-1 px-12 text-center rounded-[16px] bg-slate-950 m-auto mt-32">
+    <div className="rate w-fit py-1 px-12 text-center rounded-[16px] bg-slate-950 m-auto mt-32">
         <p className="text-lg font-semibold">Rate</p>
         <img src={cookie} alt="image de cookie" className="w-8 m-auto my-1"/>
         <p className="text-sm font-semibold">{bonus}</p>
